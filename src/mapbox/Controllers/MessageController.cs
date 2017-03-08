c390b3c974af319d9de29169dea7f667174e55aa
Anne <antastic461@gmail.com>
2017-03-07 16:47:50 -0800

    add ajax call for create

--- src/mapbox/Controllers/MessageController.cs
+++ src/mapbox/Controllers/MessageController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mapbox.Models;
+using System.Diagnostics;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,14 +17,21 @@ namespace mapbox.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            return View(db.Messages.ToList());
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
         [HttpPost]
         public IActionResult newMessage(Message message)
         {
+            Debug.WriteLine(message);
             db.Messages.Add(message);
             db.SaveChanges();
-            return Json(message);
+            Debug.WriteLine("HERE");
+            return View("Index");
         }
     }
 }
